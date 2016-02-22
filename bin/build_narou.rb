abdda13939fa4d5572de2eabd53376d7ae99e10f
io-monad <iride.monad@gmail.com>
2016-02-23 02:59:17 +0900

    Linting text at build

--- bin/build_narou.rb
+++ bin/build_narou.rb
@@ -12,11 +12,31 @@ def narou_markup(original)
   end
 end
 
+def lint_novel(text)
+  line_no = 1
+  errors = []
+  text.each_line do |line|
+    line.scan(%r{(\A[^#/ 　「『\n])|([　 ]\n?\z)|(。」)}) do |lead, trail, fs|
+      errors << "Line #{line_no}: No leading space" if lead
+      errors << "Line #{line_no}: Extra tailing space" if trail
+      errors << "Line #{line_no}: Extra full-stop in quote" if fs
+    end
+    line_no += 1
+  end
+  errors
+end
+
 def convert_file(orig_path, out_path)
   out_path.parent.mkpath
   original = IO.read(orig_path)
-  modified = narou_markup(original)
-  IO.write(out_path, modified)
+  errors = lint_novel(original)
+  if errors.empty?
+    modified = narou_markup(original)
+    IO.write(out_path, modified)
+  else
+    warn "ERROR: #{orig_path.relative_path_from(ROOT_DIR)}"
+    errors.each { |e| warn " * #{e}" }
+  end
 end
 
 if ARGV.empty?
