e49c1f03d82fcf5d2bde00c384675467189d08d2
io-monad <iride.monad@gmail.com>
2016-02-22 13:35:49 +0900

    Change .rb indent width 4 => 2

--- bin/summary.rb
+++ bin/summary.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/env ruby
 
 File.open("SUMMARY.md", "w") do |summary|
-    summary.puts "# 目次", ""
+  summary.puts "# 目次", ""
 
-    Dir.glob("chapter-*").sort.each do |chapter|
-        chapter_title = File.read("#{chapter}/.title").strip
-        summary.puts "* #{chapter_title}"
+  Dir.glob("chapter-*").sort.each do |chapter|
+    chapter_title = File.read("#{chapter}/.title").strip
+    summary.puts "* #{chapter_title}"
 
-        Dir.glob("#{chapter}/*.md").sort.each do |section|
-            section_title = File.open(section) { |f| f.gets.chomp.sub(/^#+\s*\d+\s*-\s*/, "") }
-            summary.puts "    * [#{section_title}](#{section})"
-        end
+    Dir.glob("#{chapter}/*.md").sort.each do |section|
+      section_title = File.open(section) { |f| f.gets.chomp.sub(/^#+\s*\d+\s*-\s*/, "") }
+      summary.puts "    * [#{section_title}](#{section})"
     end
+  end
 end
