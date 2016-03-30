b2d5bb3b278b15a9635565d45c0b7c0d25c815ab
io-monad <iride.monad@gmail.com>
2016-03-31 03:59:16 +0900

    Add custom style to chapter title

--- tasks/gh-pages/assets/page.js
+++ tasks/gh-pages/assets/page.js
@@ -4,9 +4,25 @@ jQuery(document).ready(function ($) {
   if (location.hash && /^#[^<>"'&]+$/.test(location.hash)) {
     var $anchor = $(location.hash);
     if ($anchor.length > 0) {
-      var targetTop = $anchor.offset().top - 100;
+      var targetTop = $anchor.offset().top;
+      if (!$anchor.is("a.anchor")) targetTop -= 150;
       $("html, body").animate({ scrollTop: targetTop });
       $anchor.parents().andSelf().addClass("target");
     }
   }
+
+  $("h1, h2").contents()
+  .filter(function () { return this.nodeType === 3 })
+  .each(function () {
+    var matched = /^\s*Ch.(\d+)\s*-\s*(.+?)\s*$/.exec(this.nodeValue);
+    if (matched) {
+      var title = matched[2].split(/\s+/).map(function (line) {
+        return $("<span>").addClass("chapter-title-line").text(line);
+      });
+      $("<div/>").addClass("chapter clearfix")
+        .append($("<div>").addClass("chapter-number").text(matched[1]))
+        .append($("<div>").addClass("chapter-title").html(title))
+        .replaceAll(this);
+    }
+  });
 });
